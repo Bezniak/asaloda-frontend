5b1f36b3a3caea90e0825faf064bd3e8304f1658
Ivan Bezniak <ivan.bezniak@gmail.com>
2024-06-21 00:34:22 +0300

    7 commit Home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -135,7 +135,7 @@ const Navbar = () => {
 
                 {isProgramDropdownVisible && (
                     <div id="mega-menu-full-dropdown" ref={dropdownRef}
-                         className="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+                         className="border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
                         <div
                             className="grid max-w-screen-xl px-4 py-1 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
                             <ul>
