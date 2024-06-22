42be4bea8efa10aa7461da8cb4f303353d8b6bdd
Ivan Bezniak <ivan.bezniak@gmail.com>
2024-06-22 20:22:33 +0300

    9 commit add ProgramSelector.jsx

--- src/components/MainPhotoBlockContainer/MainPhotoBlock.jsx
+++ src/components/MainPhotoBlockContainer/MainPhotoBlock.jsx
@@ -13,17 +13,17 @@ const MainPhotoBlock = ({
                             button
                         }) => {
     return (
-        <div className='flex items-center justify-center py-16 md:py-32 p-6 mt-14' style={{backgroundColor: bgColor}}>
+        <div className='flex items-center justify-center py-16 md:py-32 p-6 md:mt-14' style={{backgroundColor: bgColor}}>
             <div className="flex flex-col gap-5 items-center md:flex-row md:max-w-5xl w-full">
-                <div className="flex flex-col justify-center p-4 w-full md:w-1/2 text-center md:text-left">
-                    <h1 className="mb-5 text-4xl md:text-5xl font-bold text-white text-left" style={{color: colorText}}>
+                <div className="flex flex-col md:justify-center p-4 w-full md:w-1/2 text-center md:text-left">
+                    <h1 className="mb-5 text-4xl md:text-5xl font-bold text-white md:text-left xs:text-center" style={{color: colorText}}>
                         {title}
                     </h1>
-                    <h1 className="mb-10 text-4xl md:text-5xl font-bold text-white text-left"
+                    <h1 className="mb-10 text-4xl md:text-5xl font-bold text-white md:text-left xs:text-center"
                         style={{color: colorText}}>
                         {subTitle}
                     </h1>
-                    <p className="mb-6 font-normal text-white text-left" style={{color: colorText}}>
+                    <p className="mb-6 font-normal text-white md:text-left xs:text-center" style={{color: colorText}}>
                         {description}
                     </p>
                     <div className="flex justify-center md:justify-start space-x-4 mb-10">
@@ -40,7 +40,9 @@ const MainPhotoBlock = ({
                             src={smallPhoto3} alt="photo1"
                         />
                     </div>
-                    {button}
+                    <div>
+                        {button}
+                    </div>
                 </div>
                 <div className="flex justify-center items-center w-full md:w-1/2 mt-8 md:mt-0">
                     <img className="object-cover rounded-lg w-3/4 md:w-full md:h-auto"
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '320px',
+        // => @media (min-width: 320) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       colors: {
         khaki: {
           DEFAULT: '#F0E68C',
@@ -14,7 +33,7 @@ export default {
           '200': '#F5F3D9',
           '300': '#EFECB8',
           '400': '#E8E48D',
-          '500': '#F0E68C', // main khaki color
+          '500': '#F0E68C', // основной цвет khaki
           '600': '#E0D672',
           '700': '#C5BB60',
           '800': '#A09A52',
@@ -24,4 +43,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
