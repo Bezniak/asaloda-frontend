48733704f4e4dd377ca160c87122bd61a8638958
Ivan Bezniak <ivan.bezniak@gmail.com>
2024-07-08 21:27:50 +0300

    23 commit Program.jsx added

--- src/components/Program/Program.jsx
+++ src/components/Program/Program.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 import useFetchAllData from "../../api/useFetchAllData.js";
+import {Preloader} from "../Preloader/Preloader.jsx";
 
 const Program = () => {
     const id = useParams().id;
@@ -8,15 +9,23 @@ const Program = () => {
 
     console.log(data);
 
+    if (loading) return <Preloader/>
+
     return (
-        <div className='w-full h-screen flex items-center justify-center'
-             style={{backgroundColor: `${data?.attributes?.bg_color}`}}>
-            <div className='w-full max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-10'>
+        <div className='w-full md:h-80vh xs:h-auto flex items-center justify-center'
+             style={{backgroundColor: `${data?.attributes?.bg_color}`}}
+        >
+            <div className='w-full max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-10 xs:pt-5 xs:pb-10'>
                 <div className='flex-1 text-center md:text-left p-5 md:p-10'>
-                    <h1 className='text-5xl text-left font-bold text-white'>{data?.attributes?.title}</h1>
-                    <p className='mt-4 text-left text-3xl text-white font-medium'>{data?.attributes?.kcal}</p>
+                    <h1 className='md:text-5xl xs:text-2xl md:text-left xs:text-center font-bold text-white'>
+                        {data?.attributes?.title}
+                    </h1>
+                    <p className='mt-4 md:text-left xs:text-center md:text-3xl xs:text-1xl text-white font-medium'>
+                        {data?.attributes?.kcal}
+                    </p>
                     <div
-                        className='mt-8 flex flex-col md:flex-row items-center justify-center md:justify-around text-white border border-gray-500 rounded-md'>
+                        className='mt-8 xs:w-fit mx-auto flex flex-col md:flex-row flex-wrap items-center justify-center md:justify-around text-white border border-gray-500 rounded-md'
+                    >
                         <div
                             className='p-5 text-center md:text-left border-b md:border-b-0 md:border-r border-gray-500'
                         >
@@ -30,7 +39,8 @@ const Program = () => {
                             <p className='text-base	font-bold'>{data?.attributes?.one_week_price} BYN</p>
                         </div>
                         <div
-                            className='p-5 text-center md:text-left border-b md:border-b-0 md:border-r border-gray-500'>
+                            className='p-5 text-center md:text-left border-b md:border-b-0 md:border-r border-gray-500'
+                        >
                             <p className='text-sm text-gray-700 font-semibold mb-2'>Цена за 2 недели</p>
                             <p className='text-base	font-bold'>{data?.attributes?.two_week_price} BYN</p>
                         </div>
@@ -47,7 +57,7 @@ const Program = () => {
                             <p className='text-base	font-bold'>{data?.attributes?.four_week_price} BYN</p>
                         </div>
                     </div>
-                    <div className="flex md:justify-start xs:justify-center space-x-4 mt-10">
+                    <div className="flex md:justify-start xs:justify-center md:space-x-10 xs:gap-4 mt-10">
                         {data?.attributes?.img_collection?.data.map((item, index) => (
                             <img src={import.meta.env.VITE_UPLOAD_URL + item?.attributes?.url}
                                  alt={item?.attributes?.name}
